2ffe3f2e7707cec72077acb31a0636faaa02e190
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2015-01-22 20:52:14 +1000

    stuff

---
+++ admin/admin.R
@@ -0,0 +1,31 @@
+# compile c++ attributes
+library(Rcpp)
+setwd('C:/Users/jeff/Documents/GitHub/marxan')
+compileAttributes()
+
+# document code
+setwd('C:/Users/jeff/Documents/GitHub/marxan')
+library(devtools)
+library(roxygen2)
+document()
+
+# find obvious errors
+setwd('C:/Users/jeff/Documents/GitHub/marxan')
+library(devtools)
+library(roxygen2)
+load_all() 
+
+# formal package tests
+setwd('C:/Users/jeff/Documents/GitHub/marxan')
+library(devtools)
+library(roxygen2)
+test()
+
+# local install
+library(devtools)
+install('C:/Users/jeff/Documents/GitHub/marxan')
+
+
+# local from github
+library(devtools)
+install_github('paleo13/marxan')
