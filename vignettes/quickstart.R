ccc9d82e986ccee38b92d11306a6469c56d26df8
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2015-01-24 13:18:23 +1000

    data simplifcation

--- vignettes/quickstart.R
+++ vignettes/quickstart.R
@@ -12,3 +12,15 @@ plot(taspu)
 # plot vegetation data
 plot(tasinvis)
 
+## ------------------------------------------------------------------------
+# print data in the attribute table for first 20 planning units
+head(taspu@data)
+
+# plot planning units with colours indicating cost
+spplot(taspu, 'cost')
+
+# plot planning units with colors indicating status
+# units with a status of 2 have most of their area in IUCN protected areas,
+# otherwise they have a status of 0
+spplot(taspu, 'status')
+
